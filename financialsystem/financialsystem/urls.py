be0e1a2cfe4a5c7178149573803126df0801f53a
oscar3873 <oscarvargas3873@gmail.com>
2023-01-24 19:01:53 -0300

    create url, add in setting registration and core, created folders template and files html

--- financialsystem/financialsystem/urls.py
+++ financialsystem/financialsystem/urls.py
@@ -17,5 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("core.urls")),
     path("admin/", admin.site.urls),
+    # Paths de Auth
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
