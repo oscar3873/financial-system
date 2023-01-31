85559e456d788bda6df2e9874bf12eb575b3fb02
oscar3873 <oscarvargas3873@gmail.com>
2023-01-31 19:16:02 -0300

    added footer and example main

--- financialsystem/financialsystem/settings.py
+++ financialsystem/financialsystem/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #registration Priority 1 for other applications
     "registration",
+    "fontawesomefree",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
