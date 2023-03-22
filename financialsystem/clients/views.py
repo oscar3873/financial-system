7ee1a0c8400bd83354e4eac627ad0fedb096ddb1
CoFranso <cofranso.25@gmail.com>
2023-03-22 18:08:57 -0300

    hotfixs

--- financialsystem/clients/views.py
+++ financialsystem/clients/views.py
@@ -218,7 +218,7 @@ class ClientDetailView(DetailView):
             dicc[key] = value
         
         context["client_payment"] = dicc
-        
+        print (dicc)
         return context
 
     def get_object(self):
