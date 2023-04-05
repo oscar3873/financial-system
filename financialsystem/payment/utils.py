4704460646cf8bfbfce0f2dc2377d893196d5aea
CoFranso <cofranso.25@gmail.com>
2023-04-05 03:21:45 -0300

    hotfixs

--- financialsystem/payment/utils.py
+++ financialsystem/payment/utils.py
@@ -1,5 +1,5 @@
 import datetime
-from decimal import Decimal
+from django.core.exceptions import ObjectDoesNotExist
 from payment.models import Payment
 from credit.models import Installment
 
@@ -29,26 +29,39 @@ def pay_installment(payment, installments, amount_paid):
     Metodo para pagos parciales: a travez de un monto X, realiza el pago de la cuota que encaja
     y realiza la disminucion de la cuota a pagar en caso que no supere el monto de la cuota
     """
-    for index, installment in enumerate(list(installments)):
+    for i, installment in enumerate(installments):
         if installment.amount <= amount_paid:
-            print('############PAGADA COMPLETA')
+            print(f'############ Cuota {i+1} PAGADA COMPLETA')
             installment.condition = 'Pagada'
-            installment.is_paid_installment = True
             installment.payment_date = payment.payment_date
             installment.save()
 
             payment_create(payment, installment)
             amount_paid -= installment.amount
 
-        elif amount_paid >= Decimal(installment.amount/Decimal(2)):
-            print('########## PAGO PARCIAL')
-            if installment == installments.last():
-                payment.amount = amount_paid
-                installment.amount -= payment.amount
-                
-                fifteen_later = datetime.date.today() + datetime.timedelta(days=15)
-                installment.end_date = fifteen_later
-                installment.save()
-                
-                payment_create(payment, installment)
-                amount_paid = 0
+        else:
+            print(f"######### Cuota {i+1} ACUMULA MONTO A CUOTA {len(installments)}")
+            installment.amount = 0
+            installment.condition = 'Pagada'
+            installment.payment_date = payment.payment_date
+            installment.save()
+
+            payment_create(payment, installment)
+            debt = amount_paid + sum([inst.original_amount for inst in installments[i:]])
+            last_inst = installments[-1]
+            last_inst.original_amount += debt
+            fifteen_later_din(last_inst)
+            make_paid(last_inst, payment)
+            amount_paid = 0
+            break
+        
+
+def fifteen_later_din(installment):
+    fifteen_later = datetime.date.today() + datetime.timedelta(days=15)
+    installment.end_date = fifteen_later
+    installment.save()
+
+def make_paid(installment,payment):
+    installment.condition = 'Pagada'
+    installment.payment_date = payment.payment_date
+    installment.save()
