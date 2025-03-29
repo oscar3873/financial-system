3baeebaaeacbdbf236206c44414dea7aeb387e70
Oscar <oscarvargas3873@gmail.com>
2025-03-29 13:30:41 -0300

    ========CONFIGURACION PARA SERVIDOR LOCAL========

--- financialsystem/financialsystem/settings.py
+++ financialsystem/financialsystem/settings.py
@@ -34,9 +34,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["www.finanx.com.ar", 'finanx.com.ar']
+ALLOWED_HOSTS = ["www.finanx.com.ar", 'finanx.com.ar', 'www.finanx.saltacloud.com', 'finanx.saltacloud.com']
 
 
 # Application definition
@@ -170,10 +170,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/finanx/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/finanx/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
